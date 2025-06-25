bcc0bdd92fdca592f8e9b18d06332c689b71b4b8
LXSCA <lucas.edr27@gmail.com>
2025-06-25 20:31:18 -0300

    tests: fix the error message

--- tests/BankSystem.Domain.Tests/PasswordTests.cs
+++ tests/BankSystem.Domain.Tests/PasswordTests.cs
@@ -52,7 +52,7 @@ namespace BankSystem.Domain.Tests
         {
             string password = "J=`13Vf_q'8l0Ci>|4!wO193,,9uD3;7[Gy'gjJ%z;9]T_193daczxvvdcbs1349qweadsreuD3;7[Gy'gjJ%z;9]T^\\KxJ";
             var exception = Assert.Throws<WeakPasswordException>(() => Password.Create(password));
-            Assert.Equal("Senha fraca. Senha não pode exceder 64 caracteres.", exception.Message);
+            Assert.Equal("A senha não pode exceder 64 caracteres.", exception.Message);
         }
 
         [Theory]
