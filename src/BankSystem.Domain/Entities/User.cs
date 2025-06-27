0360588e0ec55fa6c18c8db76c4128b6b1d970a8
LXSCA <lucas.edr27@gmail.com>
2025-06-27 18:45:49 -0300

    feat: create user entity

---
+++ src/BankSystem.Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+using BankSystem.Domain.ValueObjects;
+
+namespace BankSystem.Domain.Entities;
+
+public class User : Entity
+{
+    public Email Email { get; private set; }
+    public Password Password { get; private set; }
+    public Guid UserType { get; private set; }
+    public Guid CustomerId { get; private set; }
+
+    private User(string email, string plainTextPassword, Guid userType, Guid customerId)
+    {
+        Email = Email.Create(email);
+        Password = Password.Create(plainTextPassword);
+        UserType = userType;
+        CustomerId = customerId;
+    }
+    
+    public static User Create(string email, string plainTextPassword, Guid userType, Guid customerId)
+        => new(email,  plainTextPassword, userType, customerId);
+
+    public void UpdateUserType(Guid userType)
+        => UserType = userType;
+    
+    public void UpdateEmail(string email) 
+        => Email = Email.Create(email);
+    
+    public void UpdatePassword(string password)
+        => Password = Password.Create(password);
+}
